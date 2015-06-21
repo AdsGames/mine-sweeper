9255f5a5e947e278de3d09c46750cc1dfe7aabfb
Allan Legemaate <alegemaate@gmail.com>
2015-06-20 22:52:23 -0400

    More bugs than features..
    -Maps sizable

--- block.cpp
+++ block.cpp
@@ -2,31 +2,53 @@
 
 using namespace std;
 
+// Create
 Block::Block(){
 
 }
 
+// Destroy!
 Block::~Block(){
   delete [] images[0][0], images[0][1];
 }
 
-void Block::SetImages( char image1[], char image2[]){
- images[0][0]= load_bitmap(image1, NULL);
- images[0][1]= load_bitmap(image2, NULL);
- flag = load_bitmap("images/blocks/flag.bmp" , NULL);
- flaged = false;
+// Load 2 images ( animation)
+void Block::SetImages( string image1, string image2){
+  images[0][0]= load_bitmap(image1.c_str(), NULL);
+  images[0][1]= load_bitmap(image2.c_str(), NULL);
+  flag = load_bitmap("images/blocks/flag.png" , NULL);
+  flaged = false;
+  animated = true;
+}
+
+// Load 1 image
+void Block::SetImages( string image1){
+  images[0][0]= load_bitmap(image1.c_str(), NULL);
+  flag = load_bitmap("images/blocks/flag.png" , NULL);
+  flaged = false;
+  animated = false;
 }
 
 int Block::GetType(){
   return type;
 }
 
+// X/Y
 int Block::GetX(){
   return x;
 }
 
 int Block::GetY(){
   return y;
+}
+
+// Width/height
+int Block::GetHeight(){
+  return height;
+}
+
+int Block::GetWidth(){
+  return width;
 }
 
 bool Block::GetSelected(){
@@ -37,64 +59,73 @@ bool Block::GetFlaged(){
   return flaged;
 }
 
+// Get that image
 BITMAP* Block::GetImage(){
   return images[0][0];
 }
 
+// Set the type
 void Block::SetType(int newType){
   type = newType;
 }
 
+// Set whether already selected
 void Block::SetSelected(bool newSelected){
   selected = newSelected;
 }
 
+// Set whether flagged or not
 void Block::SetFlaged(bool newFlag){
   flaged = newFlag;
 }
 
+// SetX
 void Block::SetX( int newValue){
   x = newValue;
 }
 
+// Set Y
 void Block::SetY( int newValue){
   y = newValue;
 }
 
-void Block::draw(BITMAP* tempBitmap){
-  if(frame==0||frame==1||frame==2||frame==3||frame==4||frame==5||frame==6||frame==7){
+// Setter for width
+void Block::SetWidth(int newValue){
+  width = newValue;
+}
+
+void Block::SetHeight(int newValue){
+  height = newValue;
+}
+
+// Draw
+void Block::draw(BITMAP* tempBitmap, float stretchWidth, float stretchHeight){
+  if( !animated||frame==0||frame==1||frame==2||frame==3||frame==4||frame==5||frame==6||frame==7){
     frame += 1;
-    DrawNewSprite( tempBitmap, images[0][0]);
+    DrawNewSprite( tempBitmap, images[0][0], stretchWidth, stretchHeight);
   }
-  else if(frame==8||frame==9||frame==10||frame==11||frame==12||frame==13||frame==14||frame==15){
+  else if( frame==8||frame==9||frame==10||frame==11||frame==12||frame==13||frame==14||frame==15){
     frame+=1;
-    DrawNewSprite( tempBitmap, images[0][1]);
+    DrawNewSprite( tempBitmap, images[0][1], stretchWidth, stretchHeight);
     if(frame==16){
       frame=0;
     }
   }
 }
 
+
+// Load new image
 void Block::Change(){
-  if(GetType()==0){SetImages("images/blocks/10.bmp","images/blocks/10.bmp");}
-  else if(GetType()==1){SetImages("images/blocks/1.bmp","images/blocks/1.bmp");}
-  else if(GetType()==2){SetImages("images/blocks/2.bmp","images/blocks/2.bmp");}
-  else if(GetType()==3){SetImages("images/blocks/3.bmp","images/blocks/3.bmp");}
-  else if(GetType()==4){SetImages("images/blocks/4.bmp","images/blocks/4.bmp");}
-  else if(GetType()==5){SetImages("images/blocks/5.bmp","images/blocks/5.bmp");}
-  else if(GetType()==6){SetImages("images/blocks/6.bmp","images/blocks/6.bmp");}
-  else if(GetType()==7){SetImages("images/blocks/7.bmp","images/blocks/7.bmp");}
-  else if(GetType()==8){SetImages("images/blocks/8.bmp","images/blocks/8.bmp");}
-  else if(GetType()==9){SetImages("images/blocks/mine.bmp","images/blocks/mine.bmp");}
-  else if(GetType()==10){SetImages("images/blocks/10.bmp","images/blocks/10.bmp");}
+  SetImages( string( "images/blocks/" + convertIntToString(type) + ".png"));
 }
 
-void Block::DrawNewSprite( BITMAP* tempBitmap, BITMAP* spriteToDraw){
-  if(GetFlaged()!=false){
-   draw_sprite(tempBitmap, spriteToDraw, GetX(), GetY());
-   draw_sprite(tempBitmap, flag, GetX(), GetY());
+// Draw
+void Block::DrawNewSprite( BITMAP* tempBitmap, BITMAP* spriteToDraw, float stretchWidth, float stretchHeight){
+  if( GetFlaged()!=false){
+    stretch_sprite( tempBitmap, spriteToDraw, x, y, stretchWidth, stretchHeight);
+    stretch_sprite( tempBitmap, flag, x, y, stretchWidth, stretchHeight);
   }
-  else if(GetFlaged()==false){
-   draw_sprite(tempBitmap, spriteToDraw, GetX(), GetY());
+  else if( GetFlaged()==false){
+    stretch_sprite( tempBitmap, spriteToDraw, x, y, stretchWidth, stretchHeight);
   }
 }
